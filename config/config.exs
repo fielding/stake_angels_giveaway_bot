3821a49b4629358d5110de4e7790917f703d6a18
Fielding Johnston <fielding@justfielding.com>
2022-06-14 09:43:25 -0500

    heroku fun:

--- config/config.exs
+++ config/config.exs
@@ -6,7 +6,7 @@ config :whos_in_bot, port: 4000
 config :whos_in_bot,
       WhosInBot.Repo,
       url: System.get_env("DATABASE_URL"),
-      ssl: System.get_env("DB_SSL", "false") |> String.to_existing_atom()
+      ssl: System.get_env("DB_SSL", "false") |> String.to_existing_atom(),
       pool_size: 18
 
 import_config "#{Mix.env}.exs"
