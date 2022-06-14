cf63388481405e5ea9bffbf1ff62e7feeb22f330
Fielding Johnston <fielding@justfielding.com>
2022-06-14 10:07:51 -0500

    heroku fun

--- config/config.exs
+++ config/config.exs
@@ -2,11 +2,15 @@ use Mix.Config
 
 config :nadia, token: (System.get_env("BOT_TOKEN") || "")
 
+config :whos_in_bot, ecto_repos: [WhosInBot.Repo]
 config :whos_in_bot, port: 4000
+
 config :whos_in_bot,
       WhosInBot.Repo,
       url: System.get_env("DATABASE_URL"),
       ssl: System.get_env("DB_SSL", "false") |> String.to_existing_atom(),
       pool_size: 18
 
-# import_config "#{Mix.env}.exs"
+
+
+import_config "#{Mix.env}.exs"
