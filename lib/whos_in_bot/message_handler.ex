fb099e4c0f0e1d61abbdf285c0b16588ba7ee38c
Fielding Johnston <fielding@justfielding.com>
2022-06-14 11:07:25 -0500

    learn elixir lol

--- lib/whos_in_bot/message_handler.ex
+++ lib/whos_in_bot/message_handler.ex
@@ -15,17 +15,16 @@ defmodule WhosInBot.MessageHandler do
     {:error, "Not a bot command"}
   end
 
-  star = ":star: "
-  gem = ":gem: "
-  gems = String.duplicate(gem, 2);
-  stars = String.duplicate(star, 11);
-
-  header = stars <> "\n" <> gems <> "Stake Angels Giveaways" <> gems <>  "\n" <> stars
-
-
   defp execute_command(message = %{ command: "start_roll_call" }) do
+    star = ":star: "
+    gem = ":gem: "
+    gems = String.duplicate(gem, 2)
+    stars = String.duplicate(star, 11)
+    header = stars <> "\n" <> gems <> "Stake Angels Giveaways " <> gems <>  "\n" <> stars
+
     RollCall.close_existing_roll_calls(message)
     RollCall.create_roll_call(message)
+
     {:ok, header}
   end
 
